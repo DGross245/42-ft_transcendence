fa396bcf756651ba28c23a56af727dca53b8c5f9
DGross245 <103501922+DGross245@users.noreply.github.com>
2023-09-23 17:56:46 +0200

    changed NestJS ports

--- backend/nestJS/src/main.ts
+++ backend/nestJS/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   // NestFactory is used to create an instance of the NestJS application, which typically starts with the AppModule.
   // await just waits until the instance is created.
   const app = await NestFactory.create(AppModule);
-  await app.listen(8000, '0.0.0.0');
+  await app.listen(3000);
 }
 bootstrap();
 // The main.ts file serves as the entry point of the application, where you bootstrap the NestJS application and start the server.
