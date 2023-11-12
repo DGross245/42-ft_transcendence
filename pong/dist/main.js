816dd02e9cb9d762dcd8c5504a36606ac4eec38d
Daniel Gross <42dgross@gmail.com>
2023-11-12 15:30:08 +0100

    Added PongScene class, restructured code for better organization

--- pong/dist/main.js
+++ pong/dist/main.js
@@ -1,73 +1,20 @@
 import * as THREE from 'three';
-const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 0, 100);
-camera.lookAt(0, 0, 0);
-const scene = new THREE.Scene();
-const renderer = new THREE.WebGLRenderer();
+import PongScene from './PongScene.js';
+// Scene creation
+const scene = new PongScene();
+scene.initialize();
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.getElementById('pong')
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-const borderGeometry = new THREE.BoxGeometry(153, 2, 2);
-const paddleGeometry = new THREE.BoxGeometry(1, 15, 1);
-const ballGeometry = new THREE.BoxGeometry(2, 2, 2);
-const ceilingMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
-const paddleMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
-const ballMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
-const ceiling = new THREE.Mesh(borderGeometry, ceilingMaterial);
-const floor = new THREE.Mesh(borderGeometry, ceilingMaterial);
-const leftPaddle = new THREE.Mesh(paddleGeometry, paddleMaterial);
-const rightPaddle = new THREE.Mesh(paddleGeometry, paddleMaterial);
-const ball = new THREE.Mesh(ballGeometry, ballMaterial);
-const ambientLight = new THREE.AmbientLight(0x404040);
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-directionalLight.position.set(1, 1, 1).normalize();
-scene.add(rightPaddle, ceiling, leftPaddle, ball, floor, ambientLight, directionalLight);
-const borderPositionY = 43;
-const paddlePositionX = 76;
-leftPaddle.position.x = -paddlePositionX;
-rightPaddle.position.x = paddlePositionX;
-ceiling.position.y = borderPositionY;
-floor.position.y = -borderPositionY;
-document.addEventListener('keydown', onKeyDown);
-document.addEventListener('keyup', onKeyUp);
-const keys = {
-    ArrowUp: false,
-    ArrowDown: false,
-    KeyW: false,
-    KeyS: false,
-};
-function onKeyDown(event) {
-    if (event.code === 'ArrowUp' || event.code === 'ArrowDown' || event.code === 'KeyW' || event.code === 'KeyS') {
-        keys[event.code] = true;
-        event.preventDefault();
-    }
-}
-function onKeyUp(event) {
-    if (event.code === 'ArrowUp' || event.code === 'ArrowDown' || event.code === 'KeyW' || event.code === 'KeyS') {
-        keys[event.code] = false;
-        event.preventDefault();
-    }
-}
+// Main camera creation and configuration
+const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 0, 90);
+camera.lookAt(0, 0, 0);
+// Animation loop including handling events
 const animate = () => {
-    if (keys.ArrowUp) {
-        if (leftPaddle.position.y < borderPositionY - paddleGeometry.parameters.height / 2) {
-            leftPaddle.position.y += 0.5;
-        }
-    }
-    else if (keys.ArrowDown) {
-        if (leftPaddle.position.y > -borderPositionY + paddleGeometry.parameters.height / 2) {
-            leftPaddle.position.y -= 0.5;
-        }
-    }
-    if (keys.KeyW) {
-        if (rightPaddle.position.y < borderPositionY - paddleGeometry.parameters.height / 2) {
-            rightPaddle.position.y += 0.5;
-        }
-    }
-    else if (keys.KeyS) {
-        if (rightPaddle.position.y > -borderPositionY + paddleGeometry.parameters.height / 2) {
-            rightPaddle.position.y -= 0.5;
-        }
-    }
+    scene.update();
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
 };
--- pong/src/main.ts
+++ pong/src/main.ts
@@ -1,90 +1,25 @@
 import * as THREE from 'three';
+import PongScene from './PongScene.js';
 
-const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 0, 100);
-camera.lookAt(0, 0, 0);
-
-const scene = new THREE.Scene();
-const renderer = new THREE.WebGLRenderer();
+// Scene creation
+const scene = new PongScene();
+scene.initialize();
+const renderer = new THREE.WebGLRenderer({
+	canvas: document.getElementById('pong') as HTMLCanvasElement
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const borderGeometry = new THREE.BoxGeometry(153, 2, 2);
-const paddleGeometry = new THREE.BoxGeometry(1, 15, 1);
-const ballGeometry = new THREE.BoxGeometry(2, 2, 2);
+// Main camera creation and configuration
+const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.set(0, 0, 90);
+camera.lookAt(0, 0, 0);
 
-const ceilingMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
-const paddleMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
-const ballMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff })
-
-const ceiling = new THREE.Mesh(borderGeometry, ceilingMaterial);
-const floor = new THREE.Mesh(borderGeometry, ceilingMaterial);
-const leftPaddle = new THREE.Mesh(paddleGeometry, paddleMaterial);
-const rightPaddle = new THREE.Mesh(paddleGeometry, paddleMaterial);
-const ball = new THREE.Mesh(ballGeometry, ballMaterial);
-
-const ambientLight = new THREE.AmbientLight(0x404040);
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-directionalLight.position.set(1, 1, 1).normalize();
-
-scene.add(rightPaddle, ceiling, leftPaddle, ball, floor, ambientLight, directionalLight);
-
-const borderPositionY = 43;
-const paddlePositionX = 76;
-
-leftPaddle.position.x = -paddlePositionX;
-rightPaddle.position.x = paddlePositionX;
-
-ceiling.position.y = borderPositionY;
-floor.position.y = -borderPositionY;
-
-document.addEventListener('keydown', onKeyDown);
-document.addEventListener('keyup', onKeyUp);
-
-const keys = {
-  ArrowUp: false,
-  ArrowDown: false,
-  KeyW: false,
-  KeyS: false,
+// Animation loop including handling events
+const tick = () => {
+	scene.update();
+	renderer.render(scene, camera);
+	window.requestAnimationFrame(tick);
 };
 
-function onKeyDown(event: KeyboardEvent): void {
-  if (event.code === 'ArrowUp' || event.code === 'ArrowDown' || event.code === 'KeyW' || event.code === 'KeyS') {
-    keys[event.code] = true;
-    event.preventDefault();
-  }
-}
-
-function onKeyUp(event: KeyboardEvent): void {
-  if (event.code === 'ArrowUp' || event.code === 'ArrowDown' || event.code === 'KeyW' || event.code === 'KeyS') {
-    keys[event.code] = false;
-    event.preventDefault();
-  }
-}
-
-const animate = (): void => {
-  if (keys.ArrowUp) {
-    if (leftPaddle.position.y < borderPositionY - paddleGeometry.parameters.height / 2) {
-      leftPaddle.position.y += 0.5;
-    }
-  } else if (keys.ArrowDown) {
-    if (leftPaddle.position.y > -borderPositionY + paddleGeometry.parameters.height / 2) {
-      leftPaddle.position.y -= 0.5;
-    }
-  }
-  if (keys.KeyW) {
-    if (rightPaddle.position.y < borderPositionY - paddleGeometry.parameters.height / 2) {
-      rightPaddle.position.y += 0.5;
-    }
-  } else if (keys.KeyS) {
-    if (rightPaddle.position.y > -borderPositionY + paddleGeometry.parameters.height / 2) {
-      rightPaddle.position.y -= 0.5;
-    }
-  }
-
-  renderer.render(scene, camera);
-  requestAnimationFrame(animate);
-};
-
-
-animate();
+tick();
