309322919c9c072534b53a3fa2eae8532393d123
Daniel Gross <42dgross@gmail.com>
2023-12-15 17:38:24 +0100

    Fixed ball paddle and border hit detection, fixed ball behaviour

--- front-end/app/pong/oneForAll/components/Ball.tsx
+++ front-end/app/pong/oneForAll/components/Ball.tsx
@@ -5,8 +5,7 @@ import { useFrame } from '@react-three/fiber';
 import * as THREE from 'three';
 import { BottomPaddle, TopPaddle } from "./Paddle";
 
-// TODO: Border bounce logic is missing
-// TODO: Paddle collision for Top and Bottom is not working correctly
+// TODO: ADD a random ball dir
 // TODO: Rework win detection 
 // FIXME: Ball resets after resizing
  
@@ -14,9 +13,11 @@ const Ball = (props) => {
 	let ref = useRef();
 	const [isVisible, setVisibility] = useState(true);
 
-	let ball = { x: 0, y: 0, velocityX: 1.5, velocityY: 1.5, speed: 2 };
+	let ball = { x: 0, y: 0, velocityX: 0, velocityY: 1, speed: 2 };
 	const halfPaddleWidth = 4 / 2;
 	const HalfPaddleHeight = 30 / 2;
+	const HalfBorderHeight = 40 / 2;
+	const HalfBorderWidth = 4 / 2;
 	const halfBall = 2;
 
 	const updateBall = (paddlePos, direction) => {
@@ -27,6 +28,16 @@ const Ball = (props) => {
 			ball.speed += 0.2;
 		ball.velocityX = direction * ball.speed;
 		ball.velocityY = normalizedY * ball.speed;
+	}
+
+	const updateBall2 = (paddlePos, direction) => {
+		const deltaX = ball.x - paddlePos.x;
+		const normalizedX = deltaX / HalfPaddleHeight;
+
+		if (ball.speed <= 4)
+			ball.speed += 0.2;
+		ball.velocityX = normalizedX * ball.speed;
+		ball.velocityY = direction * ball.speed;
 	}
 
 	useFrame(() => {
@@ -48,24 +59,84 @@ const Ball = (props) => {
 			);
 		}
 
-		const isBorder = () => {
+		const isCollidingWithPaddle2 = (paddle: { x: number; y: number; }) => {
+			return (
+				ball.x + halfBall > paddle.x - HalfPaddleHeight &&
+				ball.x - halfBall < paddle.x + HalfPaddleHeight &&
+				ball.y - halfBall < paddle.y + halfPaddleWidth &&
+				ball.y + halfBall > paddle.y - halfPaddleWidth
+			);
+		}
 
+		const isBorder = () => {
+			if (		
+				(ball.x + halfBall > 151 - HalfBorderWidth &&
+				ball.x - halfBall < 151 + HalfBorderWidth &&
+				ball.y - halfBall < 130 + HalfBorderHeight &&
+				ball.y + halfBall > 130 - HalfBorderHeight) ||
+
+				(ball.x + halfBall > -151 - HalfBorderWidth &&
+				ball.x - halfBall < -151 + HalfBorderWidth &&
+				ball.y - halfBall < 130 + HalfBorderHeight &&
+				ball.y + halfBall > 130 - HalfBorderHeight) ||
+
+				(ball.x + halfBall > -151 - HalfBorderWidth &&
+				ball.x - halfBall < -151 + HalfBorderWidth &&
+				ball.y - halfBall < -130 + HalfBorderHeight &&
+				ball.y + halfBall > -130 - HalfBorderHeight) ||
+
+				(ball.x + halfBall > 151 - HalfBorderWidth &&
+				ball.x - halfBall < 151  + HalfBorderWidth &&
+				ball.y - halfBall < -130 + HalfBorderHeight &&
+				ball.y + halfBall > -130 - HalfBorderHeight)
+			)
+				return (true)
+			else
+				return (false)
+		}
+
+		const isBorder2 = () => {
+			if (
+				(ball.x + halfBall > 130 - HalfBorderHeight && 
+				ball.x - halfBall < 130 + HalfBorderHeight &&
+				ball.y - halfBall < 151 + HalfBorderWidth &&
+				ball.y + halfBall > 151 - HalfBorderWidth) ||
+
+				
+				(ball.x + halfBall > -130 - HalfBorderHeight &&
+				ball.x - halfBall < -130 + HalfBorderHeight &&
+				ball.y - halfBall < 151 + HalfBorderWidth &&
+				ball.y + halfBall > 151 - HalfBorderWidth) || 
+
+
+				(ball.x + halfBall > -130 - HalfBorderHeight &&
+				ball.x - halfBall < -130 + HalfBorderHeight &&
+				ball.y - halfBall < -151 + HalfBorderWidth &&
+				ball.y + halfBall > -151 - HalfBorderWidth) ||
+
+
+				(ball.x + halfBall > 130 - HalfBorderHeight && 
+				ball.x - halfBall < 130 + HalfBorderHeight &&
+				ball.y - halfBall < -151 + HalfBorderWidth &&
+				ball.y + halfBall > -151 - HalfBorderWidth)
+			)
+				return (true)
+			else
+				return (false)
 		}
 
 		if (isBorder())
+			ball.velocityX *= -1;
+		else if (isBorder2())
 			ball.velocityY *= -1;
-		else if (isCollidingWithPaddle(leftPaddlePos)) {
+		else if (isCollidingWithPaddle(leftPaddlePos))
 			updateBall(leftPaddlePos, 1);
-		}
-		else if (isCollidingWithPaddle(rightPaddlePos)) {
+		else if (isCollidingWithPaddle(rightPaddlePos))
 			updateBall(rightPaddlePos, -1);
-		}
-		else if (isCollidingWithPaddle(TopPaddlePos)) {
-			updateBall(leftPaddlePos, 0);
-		}
-		else if (isCollidingWithPaddle(BottomPaddlePos)) {
-			updateBall(rightPaddlePos, -1);
-		}
+		else if (isCollidingWithPaddle2(TopPaddlePos))
+			updateBall2(TopPaddlePos, -1);
+		else if (isCollidingWithPaddle2(BottomPaddlePos))
+			updateBall2(BottomPaddlePos, 1);
 		else if ((ball.x > 200 || ball.x < -200) && 
 			props.p2Score !== 7 &&
 			props.p1Score !== 7 &&
@@ -79,27 +150,25 @@ const Ball = (props) => {
 				props.setP3Score(props.p3Score + 1);
 			else if (ball.y > 200)
 				props.setP4Score(props.p4Score + 1);
-			if (props.p2Score === 6){
+			if (props.p2Score === 6) {
 				props.setGameOver(true);
 				props.setWinner('P2');
 				setVisibility(false);
 				return
 			}
-			else if (props.p1Score === 6)
-			{
+			else if (props.p1Score === 6) {
 				props.setGameOver(true);
 				props.setWinner('P1');
 				setVisibility(false);
 				return
 			}
-			else if (props.p3Score === 6){
+			else if (props.p3Score === 6) {
 				props.setGameOver(true);
 				props.setWinner('P3');
 				setVisibility(false);
 				return
 			}
-			else if (props.p4Score === 6)
-			{
+			else if (props.p4Score === 6) {
 				props.setGameOver(true);
 				props.setWinner('P4');
 				setVisibility(false);
@@ -111,7 +180,6 @@ const Ball = (props) => {
 			ball.velocityY = 1.5;
 			ball.speed = 2;
 		}
-
 	});
 
 	return (
