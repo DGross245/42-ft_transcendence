dcf21ccdc6d325e66e7bea6e1216cbf4345052f3
lorbke <lorbke@student.42heilbronn.de>
2023-12-26 15:21:37 +0100

    fixed by executing contract call on client

--- front-end/app/scores.tsx
+++ front-end/app/scores.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+"use client"
+
 import { ethers } from 'ethers';
 import { useWeb3ModalProvider, useWeb3ModalAccount } from '@web3modal/ethers5/react'
 import scoresAbi from '../public/scores_abi.json';
@@ -13,11 +14,11 @@ const USDCAbi = [
     "function transfer(address to, uint amount)",
     "event Transfer(address indexed from, address indexed to, uint amount)"
   ];
-  
+
   function Scores() {
     const { address, chainId, isConnected } = useWeb3ModalAccount()
     const { walletProvider } = useWeb3ModalProvider()
-  
+
     async function pushScore(){
       if (!isConnected) throw Error("User disconnected")
       if (!walletProvider) throw Error("No wallet provider found")
@@ -28,7 +29,7 @@ const USDCAbi = [
       const USDTBalance = await scoresContract.balanceOf(address)
       console.log(ethers.utils.formatUnits(USDTBalance, 18))
     }
-    
+
     return (
       <button onClick={pushScore}>Get User Balance</button>
     )
