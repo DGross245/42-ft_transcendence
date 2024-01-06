64ef629525fe5088798e6f285878998bbcbc7631
Paul Geeser <>
2024-01-06 18:43:35 +0100

    package updates

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -3,19 +3,6 @@ const nextConfig = {
 	webpack: config => {
 		config.externals.push('pino-pretty', 'lokijs', 'encoding')
 		return config
-	},
-	async headers() {
-		return [
-            {
-                source: "/api/:path*",
-                headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" },
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,UPGRADE,OPTIONS" },
-                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-                ]
-            }
-        ]
 	}
 };
 
