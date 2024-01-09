89f84e551aede487bcb165c976234107be7a1aec
Julian <92728359+Julian1612@users.noreply.github.com>
2024-01-09 18:42:43 +0100

    safe

--- front-end/app/websocketsClient.tsx
+++ front-end/app/websocketsClient.tsx
@@ -10,29 +10,45 @@ const WebSocketClient = () => {
 	const [message, setMessage] = useState('');
 	const wsclient = useWSClient();
 
+	// This function triggers the creation of a new game by calling
 	const onCreateGame = async () => {
 		if (!wsclient) return;
 		const gameId = await wsclient.createGame();
 		setGameId(gameId);
 	}
+
+	// When invoked, this function subscribes the client to a specific topic by adding
+	// a message listener using wsclient.addMessageListener.
+	// When a message is received on that topic, it updates the message state.
 	const onSubscribeTopic = () => {
 		if (!wsclient) return;
 		wsclient.addMessageListener(topic, gameId, (msg: string) => {
 			setMessage(msg);
 		});
 	}
+
+	// This function sends a message to a specific game using
+	// wsclient.emitMessageToGame.
+	// It utilizes the message, topic, and gameId states to send
+	// the message to the appropriate destination.
+
+	//!! typo !!
 	const onSendMesage = () => {
 		if (!wsclient) return;
 		wsclient.emitMessageToGame(message, topic, gameId);
 	}
 
+	// This function captures the changes in the topic input field.
+	// It updates the topic state accordingly and removes any
+	// existing message listener associated with the previous topic
+	// and game ID using wsclient.removeMessageListener.
 	const onTopicChange = (e: any) => {
 		setTopic(e.target.value);
 		if (!wsclient) return;
 		wsclient.removeMessageListener(topic, gameId);
 	}
 
-  	return (<>
+	return (<>
 		<section className='flex flex-col'>
 			<div className='flex'>
 				<input
