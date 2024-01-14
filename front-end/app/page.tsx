21a94e6e761537fb8d84f9e912d096933ea4c3f8
pauldev20 <paulgeeser@aol.com>
2024-01-14 16:06:43 +0100

    basic layout changes

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -5,6 +5,8 @@ import WSClient from "./websocketsClient";
 /*                                  Component                                 */
 /* -------------------------------------------------------------------------- */
 
+// @todo add text if no wallet is connected
+
 export default function Home() {
 	return (
 		<section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
