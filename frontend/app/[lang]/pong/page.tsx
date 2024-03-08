753bff19e87a060284e69451a5e671190afb4c34
pauldev20 <paulgeeser@aol.com>
2024-03-08 17:58:45 +0100

    added login handler for pong and ttt route

--- frontend/app/[lang]/pong/page.tsx
+++ frontend/app/[lang]/pong/page.tsx
@@ -1,10 +1,21 @@
+import { useWeb3ModalAccount } from "@web3modal/ethers5/react";
 import { PongGameState } from "./context/PongGameState";
 import { PongSocket } from "./context/PongSockets";
 import OneForAllScene from "./scene/OneForAllScene";
+import { useRouter } from "next/navigation";
 import PongScene from "./scene/PongScene";
+import { useEffect } from "react";
 
 export default function PongPage() {
+	const { isConnected } = useWeb3ModalAccount();
+	const router = useRouter();
 	const gameMode = false;
+
+	useEffect(() => {
+		if (!isConnected) {
+			router.replace('/');
+		}
+	}, [isConnected, router]);
 
 	return (
 		<div style={{ width: '100%', height: '100%', position: 'relative', overflow: 'hidden' }}>
--- frontend/app/[lang]/tic-tac-toe/page.tsx
+++ frontend/app/[lang]/tic-tac-toe/page.tsx
@@ -1,16 +1,26 @@
 "use client"
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import SelectionModal, { GameOptions } from '../modals/SelectionModal';
 import { GameState } from './context/TTTGameState';
 import { Socket } from './context/TTTSockets';
 import TTTScene from './scene/TTTScene';
+import { useWeb3ModalAccount } from '@web3modal/ethers5/react';
+import { useRouter } from 'next/navigation';
 
 export default function TicTacToePage() {
 	const [gameOptions, setGameOptions] = useState<GameOptions>({ gameMode: false, isBotActive: false, botStrength: 0.5 });
 	const [open, setOpen] = useState(true);
 	const [selected, setSelected] = useState("");
+	const { isConnected } = useWeb3ModalAccount();
+	const router = useRouter();
+
+	useEffect(() => {
+		if (!isConnected) {
+			router.replace('/');
+		}
+	}, [isConnected, router]);
 
 	const joinTheGame = (selected: string) => {
 		setSelected(selected);
