1f9df3bae474837b92b9a3edd59469f4c0d205ba
lorbke <lorbke@student.42heilbronn.de>
2024-03-09 02:21:48 +0100

    add input params, fix paddle scaling

--- cli-client/main.py
+++ cli-client/main.py
@@ -7,6 +7,8 @@ import json
 import curses
 import signal
 import time
+import math
+import argparse
 
 # -------------------------------------------------------------------------- #
 #                               Data Structure                               #
@@ -37,14 +39,21 @@ class WindowTooSmall(Exception):
 #                                  Globals                                   #
 # -------------------------------------------------------------------------- #
 
-game_id = "0"
-paddle_length = 3
-paddle_speed = 1
-score_offset = 46
-win_score = 7
+parser = argparse.ArgumentParser(description='CLI Pong Client')
+parser.add_argument('ip_addr', type=str, help='IP Address')
+parser.add_argument('port', type=str, help='Port')
+parser.add_argument('game_id', type=str, help='Game ID')
+args = parser.parse_args()
+g_game_id = args.game_id
 
-g_game_width = 300
-g_game_height = 200
+g_paddle_length = 0
+g_paddle_speed = 1
+g_score_offset = 46
+g_win_score = 7
+
+g_server_game_width = 300
+g_server_game_height = 200
+g_server_paddle_length = 35
 g_scaling_factor_width = 1
 g_scaling_factor_height = 1
 
@@ -66,14 +75,6 @@ def server_y_to_cli_y(server_y):
 def server_x_to_cli_x(server_x):
 	return (int(server_x * g_scaling_factor_width + curses.COLS // 2))
 
-def server_paddle_y_to_cli_paddle_y(server_paddle_y, cli_paddle_length):
-	cli_paddle_y = []
-	i = cli_paddle_length // 2 * -1
-	while i <= cli_paddle_length // 2:
-		cli_paddle_y.append(server_y_to_cli_y(server_paddle_y) + i)
-		i += 1
-	return cli_paddle_y
-
 def cli_y_to_server_y(cli_y):
 	server_y = (cli_y - curses.LINES // 2) / g_scaling_factor_height
 	return server_y
@@ -85,20 +86,12 @@ def init_game_state():
 			'y': 0
 		},
 		'left_paddle': {
-			'x': server_x_to_cli_x(-g_game_width // 2 + 1),
-			'y': [
-				curses.LINES // 2 - 1,
-				curses.LINES // 2,
-				curses.LINES // 2 + 1,
-			]
+			'x': server_x_to_cli_x(-g_server_game_width // 2 + 1),
+			'y': curses.LINES // 2 - g_paddle_length // 2,
 		},
 		'right_paddle': {
-			'x': server_x_to_cli_x(g_game_width // 2 - 1),
-			'y': [
-				curses.LINES // 2 - 1,
-				curses.LINES // 2,
-				curses.LINES // 2 + 1,
-			]
+			'x': server_x_to_cli_x(g_server_game_width // 2 - 1),
+			'y': curses.LINES // 2 - g_paddle_length // 2,
 		},
 		'score': {
 			'left': 0,
@@ -114,12 +107,12 @@ def init_game_state_empty():
 			'y': 0
 		},
 		'left_paddle': {
-			'x': server_x_to_cli_x(-g_game_width / 2 + 1),
-			'y': [0, 0, 0, 0, 0]
+			'x': server_x_to_cli_x(-g_server_game_width / 2 + 1),
+			'y': 0
 		},
 		'right_paddle': {
-			'x': server_x_to_cli_x(g_game_width / 2 - 1),
-			'y': [0, 0, 0, 0, 0]
+			'x': server_x_to_cli_x(g_server_game_width / 2 - 1),
+			'y': 0
 		},
 		'score': {
 			'left': 0,
@@ -131,8 +124,15 @@ def init_game_state_empty():
 def init_scaling_factors():
 	global g_scaling_factor_height
 	global g_scaling_factor_width
-	g_scaling_factor_height = curses.LINES / g_game_height / 2
-	g_scaling_factor_width = curses.COLS / g_game_width / 2
+	global g_paddle_length
+	global g_cli_game_width
+	global g_cli_game_height
+	g_scaling_factor_height = curses.LINES / g_server_game_height / 2
+	g_scaling_factor_width = curses.COLS / g_server_game_width / 2
+	while g_paddle_length % 2 == 0:
+		g_scaling_factor_height -= 0.01
+		g_scaling_factor_width -= 0.01
+		g_paddle_length = int(g_server_game_height * g_scaling_factor_height / (g_server_game_height / g_server_paddle_length))
 
 # -------------------------------------------------------------------------- #
 #                               Signal Handler                               #
@@ -158,8 +158,8 @@ sio = socketio.AsyncClient(logger=False, engineio_logger=False)
 
 async def socket_initialize():
 	try:
-		requests.get('http://localhost:3000/api/socket')
-		await sio.connect('http://localhost:3000')
+		requests.get('http://' + args.ip_addr + ':' + args.port + '/api/socket')
+		await sio.connect('http://' + args.ip_addr + ':' + args.port)
 	except Exception as e:
 		logging.error(RED + f'Error connecting to server: {e}' + RESET)
 		event_quit.set()
@@ -173,7 +173,7 @@ async def start_socketio():
 		joined_game = False
 		while not event_quit.is_set():
 			if event_game_active.is_set() and not joined_game:
-				await join_game(game_id, 'Pong', False)
+				await join_game(g_game_id, 'Pong', False)
 				joined_game = True
 			if not event_game_active.is_set():
 				joined_game = False
@@ -198,23 +198,23 @@ async def start_socketio():
 # 	logging.info(RED + f'Opponent data: {msg}' + RESET)
 # 	exit
 
-@sio.on(f'message-{game_id}-Players-{game_id}')
+@sio.on(f'message-{g_game_id}-Players-{g_game_id}')
 async def room_full(msg: str):
 	logging.debug(RED + f'Room full: {msg}' + RESET)
 	event_room_full.set()
 
-@sio.on(f'room-joined-{game_id}')
+@sio.on(f'room-joined-{g_game_id}')
 async def room_joined(num_clients: int):
 	logging.debug(RED + f'Room joined, number of clients: {num_clients}' + RESET)
 
-@sio.on(f'message-{game_id}-paddleUpdate-{game_id}')
+@sio.on(f'message-{g_game_id}-paddleUpdate-{g_game_id}')
 async def receive_paddle_data(msg: str):
 	paddle_y = json.loads(msg)
 	logging.debug(RED + f'Received Paddle Data: {paddle_y}' + RESET)
 	global g_game_state
-	g_game_state['left_paddle']['y'] = server_paddle_y_to_cli_paddle_y(int(paddle_y), paddle_length)
+	g_game_state['left_paddle']['y'] = server_y_to_cli_y(paddle_y) - math.ceil(g_paddle_length / 2)
 
-@sio.on(f'message-{game_id}-ballUpdate-{game_id}')
+@sio.on(f'message-{g_game_id}-ballUpdate-{g_game_id}')
 async def receive_ball_data(msg: str):
 	ball = json.loads(msg)
 	ball_pos = ball['position']
@@ -227,7 +227,7 @@ async def receive_ball_data(msg: str):
 	g_game_state['ball']['x'] = int(new_ball['x'])
 	g_game_state['ball']['y'] = int(new_ball['y'])
 
-@sio.on(f'message-{game_id}-player-disconnected-${game_id}')
+@sio.on(f'message-{g_game_id}-player-left-${g_game_id}')
 async def player_disconnected(msg: str):
 	logging.info(RED + f'Player disconnected: {msg}' + RESET)
 	event_quit.set()
@@ -252,7 +252,7 @@ async def send_message(msg: str, topic: str, game_id: str):
 		event_game_active.clear()
 
 async def send_paddle_data(paddle_y: str, game_id: str):
-	msg = cli_y_to_server_y(paddle_y)
+	msg = cli_y_to_server_y(paddle_y + math.ceil(g_paddle_length // 2))
 	topic = f'paddleUpdate-{game_id}'
 	await send_message(msg, topic, game_id)
 
@@ -261,16 +261,16 @@ async def send_paddle_data(paddle_y: str, game_id: str):
 # -------------------------------------------------------------------------- #
 
 def draw_paddle(stdscr, paddle, global_paddle):
-	for i in range(len(paddle['y'])):
-		if paddle['y'][i] != '|':
-			stdscr.addstr(paddle['y'][i], paddle['x'], '|')
-	if global_paddle['y'][0] != paddle['y'][0]:
-		for i in range(len(paddle['y'])):
-			stdscr.addstr(paddle['y'][i], paddle['x'], ' ')
-		for i in range(len(global_paddle['y'])):
-			paddle['y'][i] = global_paddle['y'][0] + i
-		for i in range(len(paddle['y'])):
-			stdscr.addstr(paddle['y'][i], paddle['x'], '|')
+	first = paddle['y']
+	for _ in range(g_paddle_length):
+		stdscr.addstr(first, paddle['x'], '|')
+		first += 1
+	if global_paddle['y'] != paddle['y']:
+		first = paddle['y']
+		for _ in range(g_paddle_length):
+			stdscr.addstr(first, paddle['x'], ' ')
+			first += 1
+		paddle['y'] = global_paddle['y']
 	return paddle
 
 def draw_ball(stdscr, ball, global_ball, score):
@@ -278,40 +278,36 @@ def draw_ball(stdscr, ball, global_ball, score):
 		stdscr.addstr(ball['y'], ball['x'], ' ')
 		stdscr.addstr(global_ball['y'], global_ball['x'], 'O')
 		global g_game_state
-		if global_ball['x'] < server_x_to_cli_x(g_game_width // 2 * -1 - score_offset):
+		if global_ball['x'] < server_x_to_cli_x(g_server_game_width // 2 * -1 - g_score_offset):
 			score['right'] += 1
-		elif global_ball['x'] > server_x_to_cli_x(g_game_width // 2 + score_offset):
+		elif global_ball['x'] > server_x_to_cli_x(g_server_game_width // 2 + g_score_offset):
 			score['left'] += 1
 		ball['x'] = global_ball['x']
 		ball['y'] = global_ball['y']
 	return ball
 
 def move_paddle(paddle, key):
-	offset = 0
 	if key == curses.KEY_UP:
-		offset = paddle_speed * -1
+		if paddle['y'] - g_paddle_speed >= server_y_to_cli_y(-g_server_game_height // 2) + 1:
+			paddle['y'] -= g_paddle_speed
 	elif key == curses.KEY_DOWN:
-		offset = paddle_speed
-	if offset != 0 and paddle['y'][0] + offset >= server_y_to_cli_y(-g_game_height // 2) and paddle['y'][-1] + offset <= server_y_to_cli_y(g_game_height // 2):
-		i = paddle_length // 2 * -1
-		while i <= paddle_length // 2:
-			paddle['y'][i] += offset
-			i += 1
-		asyncio.run(send_paddle_data(paddle['y'][paddle_length // 2], game_id))
+		if paddle['y'] + g_paddle_speed + g_paddle_length <= server_y_to_cli_y(g_server_game_height // 2) + 1:
+			paddle['y'] += g_paddle_speed
+	asyncio.run(send_paddle_data(paddle['y'], g_game_id))
 	return paddle
 
 def draw_score(stdscr, game_state):
 	stdscr.addstr(2, curses.COLS // 2 - 3, f"{game_state['score']['left']} : {game_state['score']['right']}")
 
 def draw_field(stdscr):
-	i = server_x_to_cli_x(-g_game_width / 2 + 1)
-	while i <= server_x_to_cli_x(g_game_width / 2 - 1):
-		stdscr.addstr(server_y_to_cli_y(-g_game_height // 2) - 1, i, '-')
-		stdscr.addstr(server_y_to_cli_y(g_game_height // 2) + 1, i, '-')
+	i = server_x_to_cli_x(-g_server_game_width / 2)
+	while i <= server_x_to_cli_x(g_server_game_width / 2):
+		stdscr.addstr(server_y_to_cli_y(-g_server_game_height // 2), i, '-')
+		stdscr.addstr(server_y_to_cli_y(g_server_game_height // 2), i, '-')
 		i += 1
-	i = server_y_to_cli_y(-g_game_height // 2)
-	while i <= server_y_to_cli_y(g_game_height // 2):
-		stdscr.addstr(i, server_x_to_cli_x(0) - 1, '|')
+	i = server_y_to_cli_y(-g_server_game_height // 2)
+	while i <= server_y_to_cli_y(g_server_game_height // 2):
+		stdscr.addstr(i, server_x_to_cli_x(0), '|')
 		i += 2
 
 def draw_box(stdscr, y, x, height, width):
@@ -377,7 +373,7 @@ def wait_loop(stdscr):
 def game_loop(stdscr):
 	global g_game_state
 	game_state = init_game_state_empty()
-	while event_game_active.is_set() and not event_quit.is_set() and g_game_state['score']['left'] < win_score and g_game_state['score']['right'] < win_score:
+	while event_game_active.is_set() and not event_quit.is_set() and g_game_state['score']['left'] < g_win_score and g_game_state['score']['right'] < g_win_score:
 		key = stdscr.getch()
 		g_game_state['right_paddle'] = move_paddle(g_game_state['right_paddle'], key)
 		game_state['left_paddle'] = draw_paddle(stdscr, game_state['left_paddle'], g_game_state['left_paddle'])
@@ -396,17 +392,15 @@ def end_loop(stdscr):
 			raise Quit
 
 def curses_thread(stdscr):
+	# raise Exception(g_game_id)
 	curses.curs_set(False)
 	stdscr.nodelay(True)
 	stdscr.keypad(True)
 	if curses.LINES < 20 or curses.COLS < 50:
-		logging.error(RED + "Terminal too small, exiting" + RESET)
-		event_quit.set()
 		raise WindowTooSmall
 	init_scaling_factors()
 	global g_game_state
 	g_game_state = init_game_state()
-	
 	funcs = [start_loop, wait_loop, draw_countdown, game_loop, end_loop]
 	for i in range(len(funcs)):
 		funcs[i](stdscr)
