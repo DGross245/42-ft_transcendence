7113b49d9d58490c8dee1a82f30a041aaa596bc2
DGross245 <103501922+DGross245@users.noreply.github.com>
2025-03-03 19:17:43 +0100

    Update nextjs.yml

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
-
 module.exports = {
 	reactStrictMode: true,
+	output: 'export',
 	webpack: config => {
-		config.externals.push('pino-pretty', 'lokijs', 'encoding')
-		return config
+	  config.externals.push('pino-pretty', 'lokijs', 'encoding')
+	  return config
 	},
 	transpilePackages: [
-		"next-i18n-router"
+	  "next-i18n-router"
 	]
 };
