3e8e0c037f392bca567cc1ac9b20bf27c0d467e5
Daniel Gross <103501922+DGross245@users.noreply.github.com>
2025-03-03 19:36:52 +0100

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
 	reactStrictMode: true,
-	output: 'export',
 	webpack: config => {
 	  config.externals.push('pino-pretty', 'lokijs', 'encoding')
 	  return config
